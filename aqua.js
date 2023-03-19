ab82ff557b7d854302dd6f4c36b83a2cfc80914c
fhruby <haroodo.7@gmail.com>
2023-03-19 12:51:35 +0900

    変数に対応

--- aqua.js
+++ aqua.js
@@ -1,9 +1,8 @@
 // A javascript engine for running Aqua on the web
-
 var var_valuebox = [
     {
         name: '$version',
-        content: '1.1',
+        content: '1.0',
         type: 'string'
     },
     {
@@ -12,106 +11,96 @@ var var_valuebox = [
         type: 'string'
     }
 ];
-
 function runAll() {
-    const aquaCode = document.getElementsByTagName('aqua');
-    let ielem;
-    for(let i = 0;i < aquaCode.length;++i) {
+    var aquaCode = document.getElementsByTagName('aqua');
+    var ielem;
+    for (var i = 0; i < aquaCode.length; ++i) {
         ielem = aquaCode[i];
         runSection(ielem.innerText);
         ielem.style.display = 'none';
     }
 }
-
 function runSection(sectionText) {
-    if(getVar('$aqua') === 'vsc') {
+    if (getVar('$aqua') === 'vsc') {
         var sectionCode = sectionText.split(';');
-    } else {
-        var sectionCode = sectionText.split('\n')
     }
-    for(let j = 0;j < sectionCode.length;++j) {
+    else {
+        var sectionCode = sectionText.split('\n');
+    }
+    for (var j = 0; j < sectionCode.length; ++j) {
         runLine(sectionCode[j]);
     }
 }
-
 function runLine(lineText) {
-    const lineCode = lineText.split(' ');
-    switch(lineCode[0]) {
+    var lineCode = lineText.split(' ');
+    switch (lineCode[0]) {
         case 'outf':
-            let echoWords = '';
-            for(let m = 1;m < lineCode.length;++m) {
+            var echoWords = '';
+            for (var m = 1; m < lineCode.length; ++m) {
                 echoWords += lineCode[m];
             }
-
             console.log(echoWords);
-
-            if(echoWords.charAt(0) == '"' && echoWords.charAt(echoWords.length - 1) == '"') {
+            if (echoWords.charAt(0) == '"' && echoWords.charAt(echoWords.length - 1) == '"') {
                 document.body.insertAdjacentHTML('beforeend', '<span class="aqua_outf">' + echoWords.substring(1, echoWords.length - 1) + '</span>');
-            } else if(getVar(echoWords) === '') {
+            }
+            else if (getVar(echoWords) === '') {
                 console.error('A type literal is not explicitly specified.\n' + lineCode[0] + ' function wants string.\n' + (echoWords.charAt(0)) + ' ' + echoWords.charAt(lineCode[1].length - 1));
-            } else {
+            }
+            else {
                 document.body.insertAdjacentHTML('beforeend', '<span class="aqua_outf">' + getVar(echoWords).charAt(0) == '"' && getVar(echoWords).charAt(getVar(echoWords).length - 1) == '"' ? getVar(echoWords).substring(1, echoWords.length - 1) : getVar(echoWords) + '</span>');
             }
             break;
         case 'var':
-            if(letVar(lineCode[2], lineCode[1])) {
+            if (letVar(lineCode[2], lineCode[1])) {
                 console.error('duplicate variable name.');
             }
             break;
         case 'set':
-            changeVar(lineCode[1], lineCode[2])
+            changeVar(lineCode[1], lineCode[2]);
         default:
             console.error(lineCode[0] + ' does not exist');
             break;
     }
 }
-
 function letVar(varName, varType) {
-    let l;
-    for(l = 0;l < var_valuebox.length;++l) {
-        if(var_valuebox[l].name === varName) {
+    var l;
+    for (l = 0; l < var_valuebox.length; ++l) {
+        if (var_valuebox[l].name === varName) {
             return true;
         }
     }
-
-    let varInit = {
+    var varInit = {
         name: varName,
         content: '',
         type: varType
     };
-
     var_valuebox.push(varInit);
     return false;
 }
-
 function changeVar(varName, varContent) {
-    let l;
-    for(l = 0;l < var_valuebox.length;++l) {
-        if(var_valuebox[l].name === varName) {
+    var l;
+    for (l = 0; l < var_valuebox.length; ++l) {
+        if (var_valuebox[l].name === varName) {
             break;
         }
     }
-
     var_valuebox[l].content = varContent;
 }
-
 function getVar(varName) {
-    let l;
-    let returns = '';
-    for(l = 0;l < var_valuebox.length;++l) {
-        if(var_valuebox[l].name === varName) {
+    var l;
+    var returns = '';
+    for (l = 0; l < var_valuebox.length; ++l) {
+        if (var_valuebox[l].name === varName) {
             returns = var_valuebox[l].content;
             break;
         }
     }
-
     return var_valuebox[l].content;
 }
-
-changeVar('$aqua',document.getElementsByName('aquascript')[0].getAttribute('content'));
-
-if(getVar('$aqua') === 'true' || getVar('$aqua') === 'vsc') {
+changeVar('$aqua', document.getElementsByName('aquascript')[0].getAttribute('content'));
+if (getVar('$aqua') === 'true' || getVar('$aqua') === 'vsc') {
     runAll();
-}  else {
+}
+else {
     console.warn('Interpreter is disabled. \nInsert "<meta name="aquascript" content="true">" in <head> of HTML');
 }
---
+++ build.bat
@@ -0,0 +1,2 @@
+@echo Copyright (c) 2023 FhRuby All right reserved.
+@tsc aqua.ts
